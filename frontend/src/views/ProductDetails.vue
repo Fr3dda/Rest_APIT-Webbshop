a8fd9aab0292fcd80d39895343d272c2d2aab765
Fredrik Lundkvist <fredrik.1994.lundkvist@gmail.com>
2022-04-02 17:54:02 +0200

    uppdaterade kod v.2

--- frontend/src/views/ProductDetails.vue
+++ frontend/src/views/ProductDetails.vue
@@ -1,17 +1,17 @@
 <template>
   <div v-if="product">
       <div class="row d-flex justify-content-center align-items-center p-5 m-4  wow fadeIn" data-wow-delay="0.2s ">
-          <div class=" col-lg-5 col-sm-9 justify-content-center">
+          <div class=" mt-5 col-lg-5 col-sm-9 justify-content-center">
               <img :src="product.image" class="img-fluid d-flex  ">
           </div>
-          <div class="col-lg-7 col-md-0 ">
+          <div class="col-lg-8 col-md-0 ">
               <h2 class="text-info h2-responsive justify-content-center text-center p-3 mt-5 ">{{product.name}}</h2>
               <p class="h4 text-danger text-center">{{product.price}} SEK</p>
               <div class="mt-3 p-2">
                   <p>{{product.desc}}</p>
               </div>
           <div class="text-end mt-5">
-        <button @click="addCart({ product, quantity })" class="btn btn-sm btn-danger btn-block-end w-50"><i class="fa-solid fa-cart-plus"></i> Add to cart</button>
+        <button @click="addCart({ product, quantity })" class="btn p-3 btn-lg btn-danger   "><i class="fa-solid fa-cart-plus"></i> Add to cart</button>
       </div>
           </div>
       </div>
